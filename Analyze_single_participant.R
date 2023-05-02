0754ac21e5bd676b8d1eb08f37390b78f535f43c
snir_barzilay <snirins@gmail.com>
2023-05-02 16:07:33 +0300

    change R files

--- Analyze_single_participant.R
+++ Analyze_single_participant.R
@@ -2,20 +2,20 @@
 # Gambling
 #####################################
 library(dplyr)
-df = read.csv("2022-07-19_1001_gambling_task.csv")
-df_gamble = df %>% filter(gamble_trial == "yes" & Block== "test")
+df = read.csv("2023-04-24_1003_gambling_task.csv")
+df_gamble = df %>% filter(!is.na(sure_option))
 
 library(ggplot2)
-ggplot(df_gamble,aes(x = trial,y = loss,color = SD,group = SD))+
- stat_summary(fun = "mean")+facet_wrap(~Mu)
+ggplot(df_gamble,aes(x = use_imagery,y = loss,color = SD,group = SD))+
+ stat_summary(fun = "mean")+facet_wrap(~side_1_stim)
   
-ggplot(df_gamble,aes(x = sure_option,y = response,color = SD,group = SD))+
-  stat_summary(fun = "mean")+facet_wrap(~Mu)
+ggplot(df_gamble,aes(x = sure_option,y = response,color = use_imagery,group = use_imagery))+
+  stat_summary(fun = "mean",position = 'dodge')+facet_wrap(SD~side_1_stim)
 
 #####################################
 # Exposure
 #####################################
-exp_df = read.csv("2022-07-19_1001_exposure_task.csv")
+exp_df = read.csv("2023-04-24_1003_exposure_task.csv")
 exp_df = exp_df%>%
   mutate(score = ifelse((match_array==1 & response == 1)|(match_array==0 & response == 0),1,0))
 library(ggplot2)
--- select_reward.R
+++ select_reward.R
@@ -1,5 +1,5 @@
 library(dplyr)
-df = read.csv("2023-03-20_test_gambling_task.csv")
+df = read.csv("2023-04-24_1003_gambling_task.csv")
 df_gamble = df %>% filter(gamble_trial == "yes")
 sample(df_gamble$loss,1)
 
