179711278b5468ce8970669ccfd918950219c9f2
snirbarz <snirins@gmail.com>
2023-05-03 11:37:37 +0300

    Change instruction photos and add participants

--- select_reward.R
+++ select_reward.R
@@ -1,5 +1,5 @@
 library(dplyr)
-df = read.csv("2023-03-20_test_gambling_task.csv")
+df = read.csv("2023-05-03_1006_gambling_task.csv")
 df_gamble = df %>% filter(gamble_trial == "yes")
 sample(df_gamble$loss,1)
 
